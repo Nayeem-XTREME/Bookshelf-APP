7cd71583be7ffb35afd036c8d0d1a5c7f09a4834
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-12-17 18:23:01 +0600

    Persist Redux store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,6 +2,31 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const saveToLocalStorage = (state) => {
+    try {
+        console.log(state);
+        localStorage.setItem('state', JSON.stringify(state));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const currentState = localStorage.getItem('state');
+        if (currentState === null)
+            return undefined;
+        return JSON.parse(currentState);
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+}
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
